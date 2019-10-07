eb6e20c9985c4fbe59025d0482f232a496085fe9
shashank <shashank.jha@comprotechnologies.com>
2019-10-07 11:50:03 +0530

    deleting secret from params

--- controllers/controller.js
+++ controllers/controller.js
@@ -12,7 +12,8 @@ exports.verifyLtiLaunch = (req,res)=>{
     // delete req.body.tool_secret;
     let action = req.body.tool_provider_url;
     let secret = config.tool_secret;
-    
+    delete req.body.tool_provider_url;
+    delete req.body.tool_secret;
     oauth_signature = oauth.hmacsign('POST', action, req.body, secret);
     if (oauth_signature == req.body.oauth_signature) {
         res.render('lti.hbs');
