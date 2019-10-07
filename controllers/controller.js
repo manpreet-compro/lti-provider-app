8972ea62b1c621bb359989b1556d808e1a2e95a3
shashank <shashank.jha@comprotechnologies.com>
2019-10-07 12:35:26 +0530

    removing signature

--- controllers/controller.js
+++ controllers/controller.js
@@ -12,16 +12,18 @@ exports.verifyLtiLaunch = (req,res)=>{
     // delete req.body.tool_secret;
     let action = req.body.tool_provider_url;
     let secret = config.tool_secret;
-    delete req.body.tool_provider_url;
-    delete req.body.tool_secret;
+    // delete req.body.tool_provider_url;
+    // delete req.body.tool_secret;
+    var oauth_signature_received = req.body.oauth_signature;
+    delete req.body.oauth_signature;
     oauth_signature = oauth.hmacsign('POST', action, req.body, secret);
-    if (oauth_signature == req.body.oauth_signature) {
+    if (oauth_signature == oauth_signature_received) {
         res.render('lti.hbs');
     }
     else {
         res.render('error.hbs', {
             signature: oauth_signature,
-            receivedSignature: req.body.oauth_signature
+            receivedSignature: oauth_signature_received
         });
     }
 }
