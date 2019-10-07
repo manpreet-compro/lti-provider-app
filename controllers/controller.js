39ab0c1b901c5268ef36f469dbd23b7c1756f713
shashank <shashank.jha@comprotechnologies.com>
2019-10-07 14:11:33 +0530

    Added basic error handling

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,18 +8,26 @@ exports.homeController = (req, res)=>{
 }
 
 exports.verifyLtiLaunch = (req,res)=>{
+    if (req.body.lti_message_type != "basic-lti-launch-request" || req.body.oauth_signature_method != "HMAC-SHA1" || req.body.lti_version != "LTI-1p0" || req.body.oauth_version != "1.0") {
+        res.render('error.hbs', {
+            badRequest: true,
+            validated: false
+        });
+    }
     let action = config.TPUrl;
     let secret = config.tool_secret;
     let oauth_signature_received = req.body.oauth_signature;
     delete req.body.oauth_signature;
     oauth_signature = oauth.hmacsign('POST', action, req.body, secret);
     if (oauth_signature == oauth_signature_received) {
-        res.render('lti.hbs');
+        res.render('lti.hbs',{
+            ltiparams: req.body
+        });
     }
     else {
         res.render('error.hbs', {
-            signature: oauth_signature,
-            receivedSignature: oauth_signature_received
+            badRequest: false,
+            validated: true
         });
     }
 }
