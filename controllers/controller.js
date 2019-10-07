844c8b8612e7a07cdf71d3b85004e1c7d0c5cf4a
shashank <shashank.jha@comprotechnologies.com>
2019-10-07 16:31:25 +0530

    Adding key to secret mapping

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,23 +10,31 @@ exports.homeController = (req, res)=>{
 exports.verifyLtiLaunch = (req,res)=>{
     if (req.body.lti_message_type != "basic-lti-launch-request" || req.body.oauth_signature_method != "HMAC-SHA1" || req.body.lti_version != "LTI-1p0" || req.body.oauth_version != "1.0") {
         res.render('error.hbs', {
-            badRequest: true,
-            validated: false
+            badRequest: true
         });
     }
+
+    if (config.consumer_keys[req.body.oauth_consumer_key]) {
+        res.render('error.hbs', {
+            keyNotFound: true
+        });
+    }
+
     let action = config.TPUrl;
-    let secret = config.tool_secret;
-    let oauth_signature_received = req.body.oauth_signature;
+    let secret = config.consumer_keys[req.body.oauth_consumer_key];
+
+    let consumer_signature = req.body.oauth_signature;
     delete req.body.oauth_signature;
-    oauth_signature = oauth.hmacsign('POST', action, req.body, secret);
-    if (oauth_signature == oauth_signature_received) {
+
+    provider_signature = oauth.hmacsign('POST', action, req.body, secret);
+
+    if (provider_signature == consumer_signature) {
         res.render('lti.hbs',{
             ltiparams: req.body
         });
     }
     else {
         res.render('error.hbs', {
-            badRequest: false,
             validated: true
         });
     }
